5a3656f7f562714b08a65c44e937b4eef62f2002
danteAdecco <dante.ortiz@adecco.com>
2023-03-23 12:55:26 -0600

    Chapter 2

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,5 +1,7 @@
 package com.h2;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -12,6 +14,14 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number * 2;
+    }
+
+    private static int add(int[] numbers) {
+        int sum = 0;
+
+        sum = Arrays.stream(numbers).sum();
+
+        return sum;
     }
 }
